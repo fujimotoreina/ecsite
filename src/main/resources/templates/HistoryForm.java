1015ba70bf24ad278f9903b7612c41307f447dd6
fujimotoreina <you@example.com>
2024-05-28 16:55:09 +0900

    Lesson_ECsite

---
+++ src/main/resources/templates/HistoryForm.java
@@ -0,0 +1,15 @@
+package templates;
+
+public class HistoryForm {
+	
+	private int userId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+}
